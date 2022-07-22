ca6d250be7513fff76e81fc709626f3bdb8f2a18
dulkifal <dulkifilibacker@gmail.com>
2022-07-22 09:46:09 +0530

    express added

---
+++ server.js
@@ -0,0 +1,58 @@
+const expess = require('express');
+const app = expess();
+const port = 3000;
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+}
+);
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/style.css'));
+}
+);
+app.get('/highScores.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '/highScores.html'));
+}
+);
+app.get('/highScore.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/highScore.css'));
+}
+);
+app.get('/highScore.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/highScore.js'));
+}
+);
+
+app.get('/game.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '/game.html'));
+}
+);
+app.get('/questions.json', (req, res) => {
+  res.sendFile(path.join(__dirname, '/questions.json'));
+}
+);
+
+app.get('/game.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/game.js'));
+}
+);
+app.get('/game.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/game.css'));
+}
+);
+app.get('/end.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '/end.html'));
+}
+);
+app.get('/end.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/end.js'));
+}
+);
+app.get('/end.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/end.css'));
+}
+);
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
